ad1b1bcd562eb507ccb14b35b20f04ce28768510
andrey.goluzin <andrey@goluzin.ru>
2022-02-10 18:16:09 +0500

    Merge branch 'git-merge'

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -3,7 +3,11 @@
 
 count=1
 for param in "$@"; do
+<<<<<<< HEAD
     echo "\$@ Parameter #$count = $param"
+=======
+    echo "Next parameter: $param"
+>>>>>>> 3c6f48d (git-rebase 2)
     count=$(( $count + 1 ))
 done
 
